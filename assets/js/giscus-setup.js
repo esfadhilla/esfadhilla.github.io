621d1b1b157975422b2fc2e7d7cfe630ef7a9c74
esfadhilla <145416875+esfadhilla@users.noreply.github.com>
2025-09-18 08:43:11 +0000

    Deploying to gh-pages from @ esfadhilla/esfadhilla.github.io@cf0ba0289c1c01e163c621fbaa281eac0f8891be 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "esfadhilla/esfadhilla-esfadhilla.github.io",
+    "data-repo": "esfadhilla/esfadhilla.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
